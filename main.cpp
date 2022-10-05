e052cb6a9eabba6a8b74a82b9e638c68b083c705
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-10-05 00:24:45 +0000

    Function prototype, header, and call laid out.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
+/*********************************************
+Description: This code converts  the  passed  number  of  seconds  into  an
+equivalent  number  of  hours,  minutes,  and  seconds.  Using  the reference
+parameters,  the  function  should  alter  the  arguments  in  the  calling
+function. Created by: Ryan Downing
+**********************************************/
 #include <iostream>
 
+using namespace std;
+
+/******************* Function Prototypes *******************/
+void time(int, int &, int &, int &);
+
+/********************* Main Code ************************/
 int main() {
-  std::cout << "Hello World!\n";
+  // main variables
+  int seconds;
+  int hours, minutes, secs;
+
+  // user input for total seconds
+  cin >> seconds;
+
+  // call function
+  time(seconds, hours, minutes, secs);
+}
+
+/****************** User Defined Functions ****************/
+void time(int seconds, int &hours, int &minutes, int &secs) {
+
+  
 }
